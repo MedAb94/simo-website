c2c9db7e68798eb77152652bc1c9ed9ae3155b11
MedAb94 <medab.vall@gmail.com>
2021-04-30 17:36:28 +0000

    serviceWorker registered

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import vuetify from './plugins/vuetify';
 import i18n from './i18n'
 import VueSmoothScroll from 'vue2-smooth-scroll'
+import './registerServiceWorker'
 Vue.use(VueSmoothScroll)
 Vue.config.productionTip = false
 
